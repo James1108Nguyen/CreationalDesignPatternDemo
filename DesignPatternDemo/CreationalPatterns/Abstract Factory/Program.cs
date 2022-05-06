275ca6792879d18b2401d840cc89f2c3b63be586
James1108Nguyen <71433464+James1108Nguyen@users.noreply.github.com>
2022-05-07 06:26:49 +0700

    Up source code

---
+++ DesignPatternDemo/CreationalPatterns/Abstract Factory/Program.cs
@@ -0,0 +1,33 @@
+﻿using FactoryMethod.AbstractFactory;
+using FactoryMethod.Animal;
+using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IAnimalFactory factory;
+
+            Random random = new Random();
+            int type = random.Next(0, 2);
+
+            if(type == 0)
+            {
+                factory = new FourLegsAnimalFactory();
+            }
+            else
+            {
+                factory = new TwoLegsAnimalFactory();
+            }
+
+            Console.WriteLine(factory.createAnimal().GetName());
+            Console.WriteLine(factory.createAnimal().GetName());
+            Console.WriteLine(factory.createAnimal().GetName());
+            Console.WriteLine(factory.createAnimal().GetName());
+            Console.WriteLine(factory.createAnimal().GetName());
+        }
+    }
+}
---
+++ DesignPatternDemo/CreationalPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using FactoryMethod.Animal;
+using FactoryMethod.Factory;
+using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IAnimalFactory factory;
+
+            Random random = new Random();
+            int type = random.Next(0, 2);
+
+            if(type == 0)
+            {
+                factory = new RandomAnimalFactory();
+            }
+            else
+            {
+                factory = new BasicAnimalFactory();
+            }
+
+            Console.WriteLine(factory.createAnimal().GetName());
+            Console.WriteLine(factory.createAnimal().GetName());
+            Console.WriteLine(factory.createAnimal().GetName());
+            Console.WriteLine(factory.createAnimal().GetName());
+            Console.WriteLine(factory.createAnimal().GetName());
+        }
+    }
+}
